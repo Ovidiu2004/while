98363a7bddaac09ddf5e20c5f20ef75a46ad6c40
Frecautanu Ovidiu <70950196+Ovidiu2004@users.noreply.github.com>
2020-10-23 07:45:17 +0300

    Add files via upload

---
+++ 1while.py
@@ -0,0 +1,9 @@
+a=0
+b=0
+c=1
+   while a!=0:
+    a=eval(input("cifra data este:"))
+    b=a+c
+    c=b
+    print(b)
+print(b)
---
+++ 2while.py
@@ -0,0 +1,8 @@
+a=0
+b=0
+while (a%2==0) and (a%3!=0):
+    a=eval(input("dati numarul"))
+    if a%2==0:
+       b=b+a
+
+print(b)
